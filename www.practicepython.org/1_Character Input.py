83173be795170126fa6d2a34ec68e9d4dbb0adc4
kovarikd <dave.kovi@gmail.com>
2017-09-14 22:42:46 +0200

    Commit 2

--- www.practicepython.org/1_Character Input.py
+++ www.practicepython.org/1_Character Input.py
@@ -1,1 +1,8 @@
-input("Jak se jmenuješ")
+import datetime
+name = input("Jake je tve jmeno")
+print("Your name is " + name)
+age = int(input("Jaky je tvuj vek"))
+repeat = int(input("How many times should the output be repeated"))
+future_age = int(datetime.date.today().strftime("%Y")) + (100-age)
+for i in range(repeat):
+    print("Your age is " + str(age) + " and you'll be 100 in " + str(future_age))
