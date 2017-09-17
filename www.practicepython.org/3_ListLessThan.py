1732790e20b7e5e3dc4232dfb2af4375a6c5dcb4
kovarikd <dave.kovi@gmail.com>
2017-09-17 20:25:45 +0200

    Ex. 3 - exercise finished

--- www.practicepython.org/3_ListLessThan.py
+++ www.practicepython.org/3_ListLessThan.py
@@ -1,1 +1,9 @@
-print("hokus pokus")
+a = [1,1,2,3,5,8,13,21,34,55,89]
+output = " "
+
+number = int(input("Selected should be smaller than which number?"))
+for element in a:
+    if element < number:
+        output = output + " " + str(element)
+
+print(output)
