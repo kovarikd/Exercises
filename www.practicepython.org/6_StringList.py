71f846e85900b93ed1f8456c828e133ee66de33e
kovarikd <dave.kovi@gmail.com>
2017-09-17 22:02:26 +0200

    Ex. 6: first version

---
+++ www.practicepython.org/6_StringList.py
@@ -0,0 +1,12 @@
+word = str(input("Insert word which should be tested for palindrome:"))
+flag = False
+for c in word:
+    if not word[] == [::-c]:
+        flag = False
+    else:
+        flag = True
+
+if flag = True:
+    print("Word is palindrom")
+else:
+    print("Word is not palindrom")
